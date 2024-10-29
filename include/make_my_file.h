a07effbeac82173172231ea4349ce803ea8cc260
Mathys Cogne <mathys.cognef@gmail.com>
2024-10-29 19:51:51 +0100

    Fix version

--- include/make_my_file.h
+++ include/make_my_file.h
@@ -25,7 +25,7 @@
 # include <curl/curl.h>
 
 /***************** UPDATE *******************/
-#define CURRENT_VERSION "1.2.3"
+#define CURRENT_VERSION "1.2.4"
 #define REPO_OWNER "MathysCogne"
 #define REPO_NAME "Make_My_File-42"
 
